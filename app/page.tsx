e1f2e8d7862021550eb1ff76c17153d3555dd948
Michael Trullas Garcia <adoser2@hotmail.es>
2024-05-30 12:47:06 +0200

    File picker working

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+
+import { FilePicker } from "./ui/file-picker/file-picker";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl">Upload image</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-3xl mb-6">Upload image</h1>
+      <FilePicker uploadURL={"http://dlptest.com/http-post/"} />
     </main>
   );
 }
