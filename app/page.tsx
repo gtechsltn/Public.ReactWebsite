39bfe4ef362f17e62dfbdbc0872a6e7276a64fd0
Michael Trullas Garcia <adoser2@hotmail.es>
2024-05-30 16:32:25 +0200

    Image upload works

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-3xl mb-6">Upload image</h1>
-      <FilePicker uploadURL={"http://dlptest.com/http-post/"} />
+      <FilePicker uploadURL={"https://localhost:5001/api/v1/ImageGroup"} />
     </main>
   );
 }
