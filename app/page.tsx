8e823b6feab9f2b909c147dab78fa2bbe55b0c34
Michael Trullas Garcia <adoser2@hotmail.es>
2024-05-31 11:33:29 +0200

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import { FilePicker } from "./ui/file-picker/file-picker";
 
 export default function Home() {
+  console.log("ENV_LOCAL_VARIABLE", process.env.ENV_LOCAL_VARIABLE)
+  console.log("NEXT_PUBLIC_ENV_LOCAL_VARIABLE", process.env.NEXT_PUBLIC_ENV_LOCAL_VARIABLE)
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-3xl mb-6">Upload image</h1>
